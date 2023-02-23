5441ac213117d9cd2e0e1531a2e9c072ec63df0e
smertin123 <smertin@gmail.com>
2023-02-23 22:39:10 +1100

    da-scan output & get-creds | added local-sessions

--- domain-admins-in-non-dcs.ps1
+++ domain-admins-in-non-dcs.ps1
@@ -1,57 +1,75 @@
-$parameter = $args[0]
-
-switch ($parameter)
+switch ($args[0])
 {
-    #if argument -h show help
-    "-h" {
-        Write-Host ""
-        Write-Host "How to use this scanner:"
-        Write-Host ""
-        Write-Host "A list of Domain Admins can be obtained with:"
-        Write-Host "--da-scan: Scan for domain administrators using Get-ADGroupMember (ActiveDirectory module required)"
-        Write-Host "--da-scan-get-creds: Same as above, but request credentials"
-        Write-Host ""
-        Write-Host "To use the das in non dc checker tool:"
-        Write-Host "-l: Provide a list of domain administrators"
-        Write-Host ""
-    }
-
-    #if argument --da-scan, scan for domain admins and output to file
-    "--da-scan" {
-        Import-Module ActiveDirectory
-        $FilePath = ".\domain-admins-results.txt"
-        $DomainAdmins = Get-ADGroupMember -Identity "Domain Admins" | select -ExpandProperty "SamAccountName" | Out-File -FilePath $FilePath
-        if (.\domain-admins-results.txt) {
-            Write-Host "List of domain administrator saved to: "$FilePath
-            }  
-        }
-
-    "--da-scan-get-creds" {
-        Import-Module ActiveDirectory
-        $cred = Get-Credential
-        $FilePath = ".\domain-admins-results.txt"
-        $DomainAdmins = Get-ADGroupMember -Credential $cred -Identity "Domain Admins" | select -ExpandProperty "SamAccountName" | Out-File -FilePath $FilePath
-        if ($FilePath) {
-            Write-Host "List of domain administrator saved to: "$FilePath
-            }  
-        }
-
     #if arguments invalid, output error
     default {
         Write-Host "Use the -h flag for help"
     }
 
+    #if argument -h show help
+    "-h" {
+        Write-Host ""
+        Write-Host "This tool offers the ability to scan for Domain Administrator accounts (ActiveDirectory module required):"
+        Write-Host "--da-scan: Scan for domain administrators using current user credentials"
+        Write-Host "--get-creds: Promt for credentials"
+        Write-Host "-o: Choose output file (Default domain-admin-scan-results.txt)"
+        Write-Host ""
+        Write-Host "To use the das in non dc checker tool:"
+        Write-Host "-l: Provide a list of domain administrators"
+        Write-Host "--local-sessions: Scan the local machine for current domain administrator sessions"
+    }
+
+    #if argument --da-scan, scan for domain admins and output to file
+    "--da-scan" {
+        Import-Module ActiveDirectory
+        #if output file requested as second argument, store third argument as filename
+        if($args[1] -eq "-o") {
+            $FilePath = $args[2]
+        #if output file requested as third argument, store fourth argument as filename
+        } elseif($args[2] -eq "-o") {
+            $FilePath = $args[3]
+        } else {
+        #else use the default filename
+        $FilePath = ".\domain-admin-scan-results.txt"
+        }
+        if($args[1] -eq "--get-creds" -Or $args[3] -eq "--get-creds") {
+            $cred = Get-Credential
+            $DomainAdmins = Get-ADGroupMember -Credential $cred -Identity "Domain Admins" | select -ExpandProperty "SamAccountName" | Out-File -FilePath $FilePath
+            Write-Host "File created: "$FilePath
+        } else {
+        #get a list of admins, select the name only, output results to file 
+        $DomainAdmins = Get-ADGroupMember -Identity "Domain Admins" | select -ExpandProperty "SamAccountName" | Out-File -FilePath $FilePath
+        Write-Host "File created: "$FilePath
+        }            
+    }
+
     #if argument -l, accept a file
     "-l" {
         if ($args.Length -eq 1) {
-            throw "List of domain admins required"
+           throw "List of domain admins required"
         } else {
-            $users = Get-Content -Path $args[1]
-            # Output the users
-            foreach ($user in $users) {
-                Write-Host $user
-            }
-        }
+            $Das = Get-Content -Path $args[1]
 
+            switch ($args[2]) {
+                "--local-sessions" {
+                    $WMI = (Get-WmiObject Win32_LoggedOnUser).Antecedent
+                    $ActiveUsers = @()
+                    foreach($User in $WMI) {
+                        $StartOfUsername = $User.LastIndexOf('=') + 2
+                        $EndOfUsername = $User.Length - $User.LastIndexOf('=') -3
+                        $ActiveUsers += $User.Substring($StartOfUsername,$EndOfUsername)
+                    }
+                    $ActiveUsers = $ActiveUsers | Select-Object -Unique
+                    foreach($Da in $Das) {
+                        if ($ActiveUsers -contains $Da) {
+                        Write-Output "$Da has a current session"
+                        }
+                    }
+                }
+            }
+            <# Debug test: Output the users
+            foreach ($Da in $Das) {
+                Write-Host $Da
+            }#>
+        }
     }
 }
